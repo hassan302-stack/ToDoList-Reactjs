90909be968521cc37678d16e9f84974979294fd3
hassan302-stack <kazmihassan336@gmail.com>
2022-02-08 03:21:01 +0500

    todoList

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [additem, setadditem] = useState("");
+  const [arritem, setarritem] = useState([])
+  const add = (e) => {
+    const val = e.target.value;
+    setadditem(val)
+  }
+  const submitItem = () => {
+    setarritem((prev) => [...prev, additem])
+    document.getElementById("txt").innerHTML = " ";
+
+  }
+  const delItems = (ind) => {
+    // console.log("deleted");
+    // arritem.splice(ind, 1)
+    // submitItem();
+    // console.log(arritem);
+    // console.log(ind);
+    setarritem((prev) => { return prev.filter((value, index) => { return index !== ind }) })
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="header"><h1>ToDO List</h1></div>
+      <div className="container">
+        <div className="main">
+          <input type="text" id="txt" onChange={add} placeholder="Add a Item" />
+          <button className="add" onClick={submitItem}>+</button>
+
+        </div>
+
+        <ol>
+
+          {arritem.map((val, ind) => {
+            return (<p key={ind} id={ind}><button className="sub" onClick={() => delItems(ind)}>x</button>{val}</p>)
+          })}
+        </ol>
+      </div>
+    </>
   );
 }
 
